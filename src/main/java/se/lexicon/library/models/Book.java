3b6b1751eb6ac95e7ed9970a1a5d0f0095893863
Carlsson111 <Linusjaa1@gmail.com>
2025-01-20 11:11:09 +0100

    Part 3 fields done

--- src/main/java/se/lexicon/library/models/Book.java
+++ src/main/java/se/lexicon/library/models/Book.java
@@ -1,4 +1,8 @@
 package se.lexicon.library.models;
 
 public class Book {
+    private int id;
+    private String isbn;
+    private String title;
+    private int maxLoanDays;
 }
--- src/main/java/se/lexicon/library/models/BookLoan.java
+++ src/main/java/se/lexicon/library/models/BookLoan.java
@@ -1,4 +1,12 @@
 package se.lexicon.library.models;
 
+import java.time.LocalDate;
+
 public class BookLoan {
+    private int id;
+    private LocalDate loanDate;
+    private LocalDate dueDate;
+    private boolean returned;
+    private AppUser borrower;
+    private Book book;
 }
